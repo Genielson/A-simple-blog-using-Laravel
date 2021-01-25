9f1bbc7a3359351681331898cc411ca834d0c061
Genielson <ge.nielson@hotmail.com>
2021-01-24 23:38:31 -0300

    Fazendo um controller de exemplo

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,34 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello-world','HelloWorldController@index');
+
+Route::get('/post/${slug}', function($slug){
+    return $slug;
+})->name('post.single');
+// Rota com apelido
+
+Route::prefix('posts')->name('post.')->group(function(){
+
+    Route::get('/', 'PostController@index')->name('index');
+
+    Route::get('/create', 'PostController@create')->name('create');
+
+    Route::post('/save', 'PostController@save')->name('save');
+
+});
+
+
+Route::namespace('Admin')->prefix('admin')->group(function(){
+      
+    Route::get('/users/', 'UserController@index')->name('users.index');
+
+    Route::get('/posts/', 'PostController@index')->name('posts.index');
+
+});
+
+
+Route::get('/user/{id}', function($id){
+    return $slug;
+})->where(['id'=>'[0-9]+']);
